a3425078e9c2407fb16130fce2e0cb3349305cf0
vortax30 <sunart16up@gmail.com>
2025-05-01 10:38:20 +0700

    Update start.py

--- AyiinXd/ayiin/start.py
+++ AyiinXd/ayiin/start.py
@@ -18,7 +18,7 @@ async def startupmessage():
         if BOTLOG:
             await tgbot.send_file(
                 BOTLOG_CHATID,
-                "https://graph.org/file/1d800bed1ba868dafe081-10376ed3f68f7447d1.jpg",
+                "https://graph.org/file/d22e91671529c80c7f564-f93371b5e632c19ddd.jpg",
                 caption="𝗩𝗼𝗿𝘁𝗮𝘅-𝗨𝘀𝗲𝗿𝗯𝗼𝘁.\n     **status : Active\n     ketik `.ping` untuk cek bot!**",
                 buttons=[(Button.url("Store", "https://t.me/marknost")),
                          (Button.url("Ress", "https://t.me/resshunter"))]
