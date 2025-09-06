576e046870e756148ea064498330606fefe8be47
clawsy202 <sunart47up@gmail.com>
2025-09-07 05:02:27 +0700

    Update git.py

--- AyiinXd/core/git.py
+++ AyiinXd/core/git.py
@@ -30,7 +30,7 @@ def install_req(cmd: str) -> Tuple[str, str, int, int]:
 
 def git():
     UPSTREAM_REPO = b64decode(
-        "aHR0cHM6Ly9naXRodWIuY29tL3ZvcnRheDMwL1ZvcnRleC1Vc2VyYm90"
+        "aHR0cHM6Ly9naXRodWIuY29tL2NsYXdzeTIwMi9DbGF3c3ktVXNlcmJvdA=="
     ).decode("utf-8")
     try:
         repo = Repo()
