25fd5d2ccbfbd89b852dbad9bf7871a3ef5b88de
clawsy202 <sunart47up@gmail.com>
2025-09-07 04:55:56 +0700

    Update start.py

--- AyiinXd/ayiin/start.py
+++ AyiinXd/ayiin/start.py
@@ -18,10 +18,10 @@ async def startupmessage():
         if BOTLOG:
             await tgbot.send_file(
                 BOTLOG_CHATID,
-                "https://graph.org/file/d22e91671529c80c7f564-f93371b5e632c19ddd.jpg",
-                caption="𝗩𝗼𝗿𝘁𝗮𝘅-𝗨𝘀𝗲𝗿𝗯𝗼𝘁.\n     **status : Active\n     ketik `.ping` untuk cek bot!**",
-                buttons=[(Button.url("Store", "https://t.me/marknost")),
-                         (Button.url("Ress", "https://t.me/resshunter"))]
+                "https://graph.org/file/40dc97c81ce7e67f4dc5d-d2b60fd5a4e4965547.jpg",
+                caption="𝗖𝗹𝗮𝘄𝘀𝘆-𝗨𝘀𝗲𝗿𝗯𝗼𝘁.\n     **status : Active\n     ketik `.ping` untuk cek bot!**",
+                buttons=[(Button.url("Store", "https://t.me/minnimart")),
+                         (Button.url("LPM", "https://t.me/LPM_PLUSHIE"))]
             )
     except Exception as e:
         LOGS.error(e)
