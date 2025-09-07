bc3b5459cd156f7c4a719f0849b5768c6ef17b49
bluist202 <sunart48up@gmail.com>
2025-09-08 05:56:06 +0700

    Update startup.py

--- AyiinXd/clients/startup.py
+++ AyiinXd/clients/startup.py
@@ -18,8 +18,8 @@ from AyiinXd import (
 )
 from AyiinXd.modules.gcast import GCAST_BLACKLIST as GBL
 
-EOL = "EOL\𝗖𝗹𝗮𝘄𝘀𝘆-𝗨𝘀𝗲𝗿𝗯𝗼𝘁 {} Copyright © 2021-2025 CLAWSYUSERBOT• <https://github.com/clawsy202/Clawsy-Userbot>"
-MSG_BLACKLIST = "𝗖𝗹𝗮𝘄𝘀𝘆-𝗨𝘀𝗲𝗿𝗯𝗼𝘁𝘁 {}\nCopyright © 2021-2025 CLAWSYUSERBOT• <https://github.com/clawsy202/Clawsy-Userbot>"
+EOL = "EOL\𝗕𝗹𝘂𝗶𝘀𝘁-𝗨𝘀𝗲𝗿𝗯𝗼𝘁 {} Copyright © 2021-2025 BLUISTSERBOT• <https://github.com/bluist202/Bluist-Userbot>"
+MSG_BLACKLIST = "𝗕𝗹𝘂𝗶𝘀𝘁-𝗨𝘀𝗲𝗿𝗯𝗼𝘁 {}\nCopyright © 2021-2025 BLUISTSERBOT• <https://github.com/bluist202/Bluist-Userbot>"
 
 
 async def ayiin_client(client):
